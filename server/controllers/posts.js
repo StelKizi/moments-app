83517d077e3f0159c7935a0b251b92d644f14297
Stelkizi <stelkiziridou@gmail.com>
2021-04-07 23:12:17 +0200

    moved gitignore to parent folder

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,9 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -20,4 +20,19 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.ObjectId.isValid(_id)) {
+    return res.status(404).send('No post found with this id');
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };
